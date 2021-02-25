b768257e1826167632276809434f8a747061cd2c
Дима <kitaykin9808@gmail.com>
2021-02-26 00:16:51 +0300

    Добавьте файлы проекта.

---
+++ AlgoritmEfklida/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlgoritmEfklida
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi!\n" +
+                              "This is my demo of Euclid's algorithm.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Write numerator: ");     double numerator = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Write denumerator: ");   double denumerator = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Your fraction {0}/{1}",numerator,denumerator);
+            double a=numerator,b=denumerator;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else
+                        b = b - a;
+                }
+            }
+            Console.WriteLine("Can be abbreviated by {0}", a);
+            Console.WriteLine("Abbreviated fraction {0}/{1}",numerator/a,denumerator/a);
+            Console.ReadKey();
+        }
+    }
+}
